56c6be85c640486333fb4c1d55277a6e7a787165
eun2ce <joeun2ce@gmail.com>
2024-12-06 14:43:24 +0900

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import feedparser, datetime
+
+uri="https://eun2ce.tistory.com"
+feed = feedparser.parse(uri+"/rss")
+
+text = ""
+lst = []
+
+
+for i in feed['entries']:
+    dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
+    text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    print(i['link'], i['title'])
+
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
